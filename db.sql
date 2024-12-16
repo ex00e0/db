9b2f6fa2ac06a106a6fa3cfdafe389f4fbbbcd4a
trapbed <hasanova.leya@yandex.ru>
2024-12-16 22:15:07 +0500

    bd

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 13 2024 г., 12:34
+-- Время создания: Дек 16 2024 г., 20:14
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
 
@@ -29,11 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date_remove` date NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_remove` date DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `date_remove`, `created_at`, `updated_at`) VALUES
+(1, 'Категория 1', NULL, NULL, NULL),
+(2, 'Категория 2', NULL, NULL, NULL),
+(3, 'Категория 3', NULL, NULL, NULL),
+(4, 'Категория 4', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -43,13 +53,20 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `couriers` (
   `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` enum('Свободен','Занят') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date_remove` date NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('свободен','занят') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'свободен',
+  `date_remove` date DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `couriers`
+--
+
+INSERT INTO `couriers` (`id`, `name`, `phone`, `status`, `date_remove`, `created_at`, `updated_at`) VALUES
+(1, 'Курьер 1', '77755757575', 'свободен', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -59,7 +76,7 @@ CREATE TABLE `couriers` (
 
 CREATE TABLE `migrations` (
   `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -70,7 +87,7 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(3, '2024_12_12_111909_create_categories_table', 1),
+(3, '2024_12_12_112002_create_categories_table', 1),
 (4, '2024_12_12_112004_create_products_table', 1),
 (5, '2024_12_12_112304_create_order_products_table', 1),
 (6, '2024_12_12_112319_create_couriers_table', 1),
@@ -87,17 +104,30 @@ CREATE TABLE `orders` (
   `id` bigint UNSIGNED NOT NULL,
   `user_id` bigint UNSIGNED NOT NULL,
   `courier_id` bigint UNSIGNED NOT NULL,
-  `status` enum('Создан','Назначен','Выполнен') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `street` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `house` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('Создан','Назначен','Выполнен') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `street` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `house` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `apart` int UNSIGNED NOT NULL,
-  `sum` decimal(2,2) NOT NULL,
+  `sum` decimal(7,2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `courier_id`, `status`, `name`, `phone`, `email`, `street`, `house`, `apart`, `sum`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'Выполнен', 'имя', 'телефон', 'почта', 'улица', 'дом', 1, '20.00', '2024-11-29 12:45:27', NULL),
+(2, 1, 1, 'Выполнен', 'имя', 'телефон', 'почта', 'улица', 'дом', 1, '20.00', '2024-11-29 12:45:27', NULL),
+(3, 1, 1, 'Выполнен', 'имя', 'телефон', 'почта', 'улица', 'дом', 1, '20.00', '2024-11-29 12:45:27', NULL),
+(4, 1, 1, 'Выполнен', 'имя', 'телефон', 'почта', 'улица', 'дом', 1, '20.00', '2024-12-29 12:45:27', NULL),
+(5, 1, 1, 'Выполнен', 'имя', 'телефон', 'почта', 'улица', 'дом', 1, '20.00', '2024-06-29 12:45:27', NULL),
+(6, 1, 1, 'Выполнен', 'имя', 'телефон', 'почта', 'улица', 'дом', 1, '20.00', '2024-06-29 12:45:27', NULL),
+(7, 1, 1, 'Назначен', '-', '-', '-', '-', '-', 1, '10042.00', '2024-11-29 12:45:27', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,6 +143,16 @@ CREATE TABLE `order_products` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Дамп данных таблицы `order_products`
+--
+
+INSERT INTO `order_products` (`id`, `product_id`, `amount`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, NULL, NULL),
+(2, 1, 2, NULL, NULL),
+(3, 1, 2, NULL, NULL),
+(4, 3, 5, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -121,11 +161,11 @@ CREATE TABLE `order_products` (
 
 CREATE TABLE `personal_access_tokens` (
   `id` bigint UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `tokenable_id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -141,19 +181,31 @@ CREATE TABLE `personal_access_tokens` (
 CREATE TABLE `products` (
   `id` bigint UNSIGNED NOT NULL,
   `category_id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `current_price` decimal(10,2) NOT NULL,
-  `old_price` decimal(10,2) NOT NULL,
-  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `old_price` decimal(10,2) DEFAULT NULL,
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `weight` int UNSIGNED NOT NULL,
-  `compound` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `compound` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `new` tinyint(1) NOT NULL,
   `hit` tinyint(1) NOT NULL,
-  `discount` int UNSIGNED NOT NULL,
-  `date_remove` date NOT NULL,
+  `discount` int UNSIGNED DEFAULT NULL,
+  `date_remove` date DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `name`, `current_price`, `old_price`, `image`, `weight`, `compound`, `new`, `hit`, `discount`, `date_remove`, `created_at`, `updated_at`) VALUES
+(1, 2, 'Продукт 1', '2020.00', NULL, 'free-icon-comment-upload-17558719.png', 100, '1, 2, 3', 0, 0, NULL, NULL, NULL, NULL),
+(2, 1, 'Продукт 2', '3334.00', NULL, 'download.png', 48, '------', 0, 0, NULL, NULL, NULL, NULL),
+(3, 1, 'Продукт 3', '4338.00', NULL, 'download.png', 200, '323', 1, 0, NULL, NULL, NULL, NULL),
+(4, 4, 'Продукт 4', '10055.00', NULL, 'free-icon-comment-upload-17558719.png', 200, '-----', 0, 1, NULL, NULL, NULL, NULL),
+(5, 4, 'Продукт 5', '55.00', NULL, 'download.png', 566, '+-', 1, 1, NULL, NULL, NULL, NULL),
+(6, 3, 'Продукт 6', '233.00', NULL, 'free-icon-comment-upload-17558719.png', 482, '+_+_', 0, 0, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -163,9 +215,9 @@ CREATE TABLE `products` (
 
 CREATE TABLE `remove_passwords` (
   `id` bigint UNSIGNED NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_add` date NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -178,16 +230,23 @@ CREATE TABLE `remove_passwords` (
 
 CREATE TABLE `users` (
   `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` enum('admin','user') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','user') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user',
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Пользователь 1', 'user@user.ru', '77755757575', NULL, '$2y$10$xDBQ0.2.ruy/I/sXpdd4KuWEeDl3JYzWYjtcOFk8j73t1xn/IZqca', 'user', NULL, NULL, NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -262,13 +321,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `couriers`
 --
 ALTER TABLE `couriers`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `migrations`
@@ -280,13 +339,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `order_products`
 --
 ALTER TABLE `order_products`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `personal_access_tokens`
@@ -298,7 +357,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `remove_passwords`
@@ -310,7 +369,7 @@ ALTER TABLE `remove_passwords`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
